f41f2f3f2ded79d96ce78903c14c856dc0a57dc9
Arvind Batham <arvindbatham600@gmail.com>
2024-03-15 03:03:32 +0530

    easy done of week1

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,64 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
+  // first check if transaction array is empty
+  if (transactions.length === 0) {
+    console.log("Empty transaction array ");
+  }
+
+  // if not empty then -->
+  // initialize an object to store the category and price
+  const categoryStore = {};
+
+  transactions.forEach((transaction) => {
+    const { category, price } = transaction;
+
+    // if the category already exists, add the price to the total
+    if (categoryStore[category]) {
+      categoryStore[category] += price;
+      console.log(categoryStore);
+    } else {
+      // if the category doesn't exist, create a new one
+      categoryStore[category] = price;
+      console.log(categoryStore);
+    }
+  });
+
+  // convert object into array of objects
+  const totalSpentByCategory = Object.keys(categoryStore).map((category) => ({
+    category,
+    totalSpent: categoryStore[category],
+  }));
+
+  return totalSpentByCategory;
+
   return [];
 }
+
+let transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656076800000,
+    price: 20,
+    category: "Clothing",
+    itemName: "Shirt",
+  },
+  {
+    id: 3,
+    timestamp: 1656076800000,
+    price: 15,
+    category: "Food",
+    itemName: "Coffee",
+  },
+];
+
+console.log(calculateTotalSpentByCategory(transactions));
 
 module.exports = calculateTotalSpentByCategory;
