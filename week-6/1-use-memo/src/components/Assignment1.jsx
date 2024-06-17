b1c6e701c3390ee2d276d2e6985038d84d1834b2
Arvind Batham <arvindbatham600@gmail.com>
2024-06-17 23:39:56 +0530

    will solve this assignment

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+// first assignment.
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
